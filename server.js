94e7488f83477519fbfd3429b07579f4e0acac32
Michael Frank <frank.522@osu.edu>
2021-10-28 19:08:09 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+const port = process.env.PORT || 3000;
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+//client.connect();
+
+
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`Example app listening at ${host}:${port}`);
+});
